c38a54e21ff72b1d499deaabda312f848a26aa03
Rajendrankr25 <rajendrankr25@gmail.com>
2022-12-31 18:00:15 +0530

    CRUD with API added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { AppBar, Button, Toolbar } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { AddBook } from './AddBook';
 import './App.css';
+import { BooksList } from './BooksList';
+import { Home } from './Home';
+
+//Implement CRUD operation with different UI of admin dashboard for Library mangement and use formik for all form validations
 
 function App() {
+
+  const navigate = useNavigate();
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://63899fddc5356b25a203eddb.mockapi.io/books")
+      .then((bk) => bk.json())
+      .then((bks) => setBooks(bks));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position='static' color='secondary'>
+        <Toolbar>
+          <Button color='inherit' onClick={() => navigate("/")}>Home</Button>
+          <Button color='inherit' onClick={() => navigate("/books")}>Books</Button>
+          <Button color='inherit' onClick={() => navigate("/books/add")}>Add Books</Button>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BooksList books={books} />} />
+        <Route path="/books/add" element={<AddBook />} />
+      </Routes>
     </div>
   );
 }
